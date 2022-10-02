88346c6ed9a5ae8bd84b89abf1e39b2b69ffee27
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-02 16:36:50 +0100

    add the load balancer: using ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,4 +5,16 @@ const app = express();
 
 app.use(json());
 
-app.listen(3000, () => console.log('Listening on PORT 3000 '));
+app.get('/', (req, res) => {
+  res.send('Hi there');
+});
+
+app.get('/api/users/hello', (req, res) => {
+  res.send('api users hello!!!');
+});
+
+app.get('/api/users/test', (req, res) => {
+  res.send('Hi there000000');
+});
+
+app.listen(5000, () => console.log('Listening on PORT 5000 '));
