f62c8a02cd2c4a150e86f0042dd00cef2f2eeb54
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-02 16:43:29 +0100

    add current user route

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,20 +1,12 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
 
 const app = express();
 
 app.use(json());
 
-app.get('/', (req, res) => {
-  res.send('Hi there');
-});
-
-app.get('/api/users/hello', (req, res) => {
-  res.send('api users hello!!!');
-});
-
-app.get('/api/users/test', (req, res) => {
-  res.send('Hi there000000');
-});
+//routes
+app.use(currentUserRouter);
 
 app.listen(5000, () => console.log('Listening on PORT 5000 '));
