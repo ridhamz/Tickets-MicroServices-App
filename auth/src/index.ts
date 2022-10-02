3a60ff11d9db28a9c1bb8c06291eba6dcdaa935e
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-02 17:29:02 +0100

    Auth service: add all routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signUpRouter } from './routes/signup';
+import { signOutRouter } from './routes/signout';
 
 const app = express();
 
@@ -8,5 +11,8 @@ app.use(json());
 
 //routes
 app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signUpRouter);
+app.use(signOutRouter);
 
 app.listen(5000, () => console.log('Listening on PORT 5000 '));
