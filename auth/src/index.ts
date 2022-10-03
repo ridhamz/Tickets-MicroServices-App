ddb5ec9a0a4a568cf793c99797a6ea3a1b03f2d3
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-03 12:20:51 +0100

    Auth service: error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signUpRouter } from './routes/signup';
 import { signOutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
@@ -14,5 +16,13 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signUpRouter);
 app.use(signOutRouter);
+
+// error handler
+app.use(errorHandler);
+
+// handle not found routes
+app.get('*', () => {
+  throw new NotFoundError();
+});
 
 app.listen(5000, () => console.log('Listening on PORT 5000 '));
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
@@ -17,7 +19,7 @@ router.post(
   (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
     res.send('createing user');
