7314eaaa5f7b32b0e165657b735dab66f9ae3878
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-03 15:55:22 +0100

    Auth service: add mongodb kubernetes deployment and service

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -3,7 +3,7 @@ import { CustomError } from '../errors/custom-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 
-export function errorHandler(
+export async function errorHandler(
   err: Error,
   req: Request,
   res: Response,
