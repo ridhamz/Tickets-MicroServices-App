bd0d0c1c8dbe98ef2f373b5bd7f89c3e9dae1854
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-12 21:40:18 +0100

    Start working on the orders service

---
+++ orders/src/index.ts
@@ -0,0 +1,47 @@
+import { app } from './app';
+import mongoose from 'mongoose';
+import { natsWrapper } from './nats-wrapper';
+
+async function start() {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    await mongoose.connect('mongodb://orders-mongo-srv:27017/orders');
+    console.log('connected to the database');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+app.listen(5002, () => {
+  start();
+  console.log('Tickets service: listening on PORT 5002 ');
+});
